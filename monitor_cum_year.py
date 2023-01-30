ed3e7ff9ea8e388f165b40d9643ef7ec446c8e6c
Ed Gordijn <gordijn_e@schiphol.nl>
2023-01-30 12:14:37 +0100

    prognose toegevoegd

--- monitor_cum_year.py
+++ monitor_cum_year.py
@@ -1,10 +1,159 @@
+import numpy as np
 import pandas as pd
-import solaredge
-from datetime import datetime, time, timedelta
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
+
+import solaredge
+from ssdtools.meteo import Meteo
+
+#%% Solardata user info
+api_key = '0JZ8Q9LPBWIQ4KJHV8XJ2D1STNQA3MHH'
+site_id = '2752001'
+
+s = solaredge.Solaredge(api_key)
+
+#%% Historische jaaropbrengst
+
+#%% Correlate KNMI data with solarpanel production
+
+start_date = datetime(2022, 3, 3)
+end_date = datetime(2022, 12, 31)
+
+panels = s.get_energy_details_dataframe(site_id, 
+                                        start_time=start_date,
+                                        end_time=end_date)
+# Energy in kWh
+panels['Production'] /= 1000
+
+# Data van het KNMI
+meteo = Meteo.from_knmi(start=start_date.strftime('%Y%m%d'), 
+                        end=end_date.strftime('%Y%m%d'),
+                        vars='Q',
+                        stns=215)
+
+# Calculate daily radiation, global radiation (in J/cm2)
+straling = meteo.data.groupby(['YYYYMMDD'])['Q'].sum()
+
+# Lineair fit
+X = np.vstack([straling, np.zeros(len(straling))]).T
+a, _, _, _ = np.linalg.lstsq(X, panels['Production'], rcond=-1)
+
+#%% Correlate stn 215 Voorschoten with stn 210 Valkenburg zh
+
+start_date = datetime(2014, 8, 1)
+end_date = datetime(2016, 4, 30)
+
+meteo_210 = Meteo.from_knmi(start=start_date.strftime('%Y%m%d'), 
+                            end=end_date.strftime('%Y%m%d'),
+                            vars='Q',
+                            stns=210,
+                            hourly=False).data
+
+meteo_215 = Meteo.from_knmi(start=start_date.strftime('%Y%m%d'), 
+                            end=end_date.strftime('%Y%m%d'),
+                            vars='Q',
+                            stns=215,
+                            hourly=False).data
+
+meteo = pd.concat([meteo_210, meteo_215])
+
+# Calculate daily radiation, global radiation (in J/cm2)
+straling = meteo.groupby(['STN', 'YYYYMMDD'])['Q'].sum() * a[0]
+
+# Lineair fit
+X = np.vstack([straling.loc[210], np.zeros(len(straling.loc[210]))]).T
+p, _, _, _ = np.linalg.lstsq(X, straling.loc[215], rcond=-1)
+
+#%% Predict production based on previous radiation
+
+start_date = datetime(1988, 1, 1)
+end_date = datetime(2016, 4, 30)
+
+meteo_210 = Meteo.from_knmi(start=start_date.strftime('%Y%m%d'), 
+                            end=end_date.strftime('%Y%m%d'),
+                            vars='Q',
+                            stns=210,
+                            hourly=False).data
+# Correction for difference with stn 215
+meteo_210['Q'] *= p[0]
+
+start_date = datetime(2016, 5, 1)
+end_date = datetime(2022, 12, 31)
+
+meteo_215 = Meteo.from_knmi(start=start_date.strftime('%Y%m%d'), 
+                            end=end_date.strftime('%Y%m%d'),
+                            vars='Q',
+                            stns=215,
+                            hourly=False).data
+
+meteo = pd.concat([meteo_210, meteo_215])
+
+
+# Calculate daily radiation, global radiation (in J/cm2)
+straling = meteo.groupby(['YYYYMMDD'])['Q'].sum() * a[0]
+
+
+#%% Expected yearly distribution
+
+# Daily stats
+straling = pd.DataFrame(straling)
+straling['jaar'] = straling.index.year
+straling['maand'] = straling.index.month
+straling['dag'] = straling.index.day
+
+stats_per_dag = straling.groupby(['maand', 'dag'])['Q'].agg(['mean', 'sem'])
+stats_per_dag['ci95_hi'] = stats_per_dag['mean'] + 1.96* stats_per_dag['sem']
+stats_per_dag['ci95_lo'] = stats_per_dag['mean'] - 1.96* stats_per_dag['sem']
+
+# Cumulative value
+stats_per_dag['mean'] = stats_per_dag['mean'].cumsum()
+stats_per_dag['ci95_hi'] = stats_per_dag['ci95_hi'].cumsum()
+stats_per_dag['ci95_lo'] = stats_per_dag['ci95_lo'].cumsum()
+
+# drop schrikkeljaar
+stats_per_dag = stats_per_dag.drop((2,29))
+
+# Production this year
+panels['opgewekt'] = panels['Production'].cumsum()
+energy_ytd = panels['opgewekt'].max()
+
+panels['jaar'] = panels.index.year
+panels['maand'] = panels.index.month
+panels['dag'] = panels.index.day
+panels_per_dag = panels.groupby(['maand', 'dag'])['opgewekt'].sum()
+
+stats_per_dag = stats_per_dag.join(panels_per_dag)
+
+
+# Complate the dates
+stats_per_dag = stats_per_dag.reset_index()
+stats_per_dag['jaar'] = 2023
+
+stats_per_dag.index = pd.to_datetime({'year': stats_per_dag['jaar'],
+                                      'month': stats_per_dag['maand'],
+                                      'day': stats_per_dag['dag']})
+
+#%% plot setup
+plt.style.use('myplotstyle')
+
+fig, ax = plt.subplots()
+fig.set_size_inches(10, 6)
+   
+
+#%% Plot stats
+stats_per_dag.plot(y='mean',
+                   ax=ax)
+
+ax.fill_between(stats_per_dag.index,
+                stats_per_dag['ci95_lo'],
+                stats_per_dag['ci95_hi'],
+                color='b',
+                alpha=.1)
+
+
 
 #%% Periods
 today = datetime.now()
@@ -17,10 +166,6 @@ year_end = today # - timedelta(days=1)
 
 
 #%% Get solardata
-api_key = '0JZ8Q9LPBWIQ4KJHV8XJ2D1STNQA3MHH'
-site_id = '2752001'
-
-s = solaredge.Solaredge(api_key)
 
 # Energy this year
 sdata = s.get_energy(site_id,
@@ -38,15 +183,7 @@ df_energy['value'] = df_energy['value'].cumsum()
 
 # Total energy
 energy_ytd = df_energy['value'].max()
-
-
-#%% plot setup
-plt.style.use('myplotstyle')
-
-fig, ax = plt.subplots()
-fig.set_size_inches(10, 6)
-
-# fig.suptitle('Maandopbrengst', fontsize=16)                     
+                 
 
 #%% plot
 
